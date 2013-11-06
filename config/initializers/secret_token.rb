ec0971325807a19150e61ed1801fa0768409d4f4
Florian Sahling <sahling@prod.uni-hannover.de>
2013-11-06 13:28:13 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuNow::Application.config.secret_token = 'f82cfbad234becdf4f0abf55807fce7ff45e2114524dc6af267e0a45726af573e68d6a36a3f3ea1cff5d84495dbb221d190a5b9c1f16c1c2fc8dc01dfec6cd4e'
